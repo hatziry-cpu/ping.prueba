dc34c285debe7cc65084937229ee2d58d283bc72
hatziry-cpu <hatziry@kohmi.net>
2026-03-18 16:16:18 -0600

    Add files via upload
    jjkk

---
+++ ping.prueba-main/src/main/java/com/ejemplo/ping/prueba/controller/PingController.java
@@ -0,0 +1,48 @@
+package com.ejemplo.ping.prueba.controller;
+
+import jakarta.websocket.server.PathParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.SimpleDateFormat;
+
+@RestController
+@RequestMapping(path = "/start")
+public class PingController {
+
+    /**
+     * La siguiente es la url a utilizar en ambiente de desarrollo
+     * URL: http://localhost:30300/ejemploPing/start/ping
+     * @return
+     */
+    @GetMapping(path = "/ping")
+    public String pingGet(){
+        java.util.Date fechaPing = new java.util.Date();
+        String retorno = null;
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        retorno = "Ping recibido el dia u hora: " + sdf.format(fechaPing);
+        return retorno;
+    }
+
+
+    @GetMapping(path = "/saludo/{usuario}")
+    public String pingGet(@PathParam("usuario") String usuario){
+        java.util.Date fechaPing = new java.util.Date();
+        String retorno = null;
+
+        if(usuario==null){
+            retorno = "Debes de proporcionarme un usuario";
+            return retorno;
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        retorno = "Ping recibido el dia u hora: " + sdf.format(fechaPing) + "\n" +
+                "Saludos desde AWS el usuario: " + usuario;
+
+        return retorno;
+    }
+
+
+
+}
